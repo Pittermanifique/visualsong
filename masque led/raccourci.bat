8eaa5d3da9b66adb7a5b0c4eb5f78789020409ba
Pittermanifique <isaac.lestage@gmail.com>
2025-07-13 21:27:43 +0200

    update

--- masque led/raccourci.bat
+++
@@ -1,6 +0,0 @@
-@echo off
-set "TargetPath=%~dp0beat\beat.bat"
-set "IconPath=%~dp0beat\icone.ico"  :: chemin vers ton icône .ico
-set "ShortcutPath=%USERPROFILE%\Desktop\beat.lnk"
-
-powershell -command "$s=(New-Object -COM WScript.Shell).CreateShortcut('%ShortcutPath%');$s.TargetPath='%TargetPath%';$s.IconLocation='%IconPath%';$s.Save()"
